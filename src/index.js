29b78b8bff4eff25c3fdc11940ae28f727cfc1a0
alexalehno <alekswakeup@gmail.com>
2021-08-02 08:24:00 +0300

    feat: add solution human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(num) {
+    let arrUnits = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+
+    let arrTenth = [
+        ,
+        ,
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (num < 20) {
+        return arrUnits[num];
+    }
+
+    if (num >= 20 && num < 100) {
+        return `${arrTenth[Math.trunc(num / 10)]} ${arrUnits[num % 10]}`
+            .split("zero")
+            .join(" ")
+            .trim();
+    }
+
+    if (num >= 100 && num < 1000) {
+        let tenth;
+        let units = arrUnits[num % 10];
+
+        if (num % 100 < 20) {
+            tenth = arrUnits[num % 100];
+            units = "";
+        } else {
+            tenth = arrTenth[Math.trunc((num % 100) / 10)];
+        }
+
+        return `${arrUnits[Math.trunc(num / 100)]} hundred ${tenth} ${units}`
+            .split("zero")
+            .join(" ")
+            .trim();
+    }
+};
